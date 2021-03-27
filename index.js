2dfcdd351defcbaddb07744a65eebf1e645e74f9
Alex <alex81.dev@gmail.com>
2021-03-27 12:12:22 -0500

    first commit

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const server = http.createServer((request, response) => {
   };
 });
 
-server.listen(3000, () => {
+server.listen(3050, () => {
   console.log('Servidor operativo en pto 3000 por Localhost');
 });
